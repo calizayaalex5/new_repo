a2ddbc89410ebbf3fac392dfa357b0070f95d40c
calizayaalex5 <calizayaalex5@gmail.com>
2025-11-07 11:31:11 -0300

    update database

--- database/assigment2.sql
+++ database/assigment2.sql
@@ -1,6 +1,20 @@
-INSERT INTO account (first_name, last_name, email, password)
-VALUES ('Tony', 'Stark','tony@starkent.com', 'Iam1ronM@n');
+-- cambiar descripccion de hummer
 
-UPDATE account
-SET email = 'ironman@avengers.com'
-WHERE first_name = 'Tony' AND last_name = 'Stark';
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer' 
+
+-- seleccionar vehiculos que sean tipo sport, hay que mostrar 'make' 'model'
+
+SELECT i.inv_model, i.inv_model, c.classification_name
+FROM inventory AS i
+INNER JOIN classification AS c
+	ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport'
+
+-- add '/vehicles'
+
+UPDATE inventory
+SET
+	inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles')
